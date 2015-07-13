e69544f157f12350f11357cc04cb62492f0c732a
hualcAdhoc <hualuchi@appadhoc.com>
2015-07-13 11:10:07 +0800

    Update package.js

--- adhoc-server/package.js
+++ adhoc-server/package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'hualc:adhoc-server',
+  name: 'adhoc:adhoc-server',
   version: '0.8.2',
   summary: 'Adhoc api for A/B testing',
   git: 'https://github.com/AppAdhoc/meteor-adhoc/tree/master/adhoc-server',
